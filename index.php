0e458a5b2168968b02c9927df60c7cc65749cb24
Bas Casander <bas@breenstorm.nl>
2022-10-20 17:12:09 +0200

    p

--- index.php
+++ index.php
@@ -18,50 +18,6 @@ function findBestMatchIndex($needle,$haystack) {
     return $bestmatch;
 }
 
-class DumpHTTPRequestToFile {
-    public function execute($targetFile) {
-        $data = sprintf(
-            "%s %s %s\n\nHTTP headers:\n",
-            $_SERVER['REQUEST_METHOD'],
-            $_SERVER['REQUEST_URI'],
-            $_SERVER['SERVER_PROTOCOL']
-        );
-
-        foreach ($this->getHeaderList() as $name => $value) {
-            $data .= $name . ': ' . $value . "\n";
-        }
-
-        $data .= "\nRequest body:\n";
-
-        file_put_contents(
-            $targetFile,
-            $data . file_get_contents('php://input') . "\n"
-        );
-
-        echo("Done!\n\n");
-    }
-
-    private function getHeaderList() {
-        $headerList = [];
-        foreach ($_SERVER as $name => $value) {
-            if (preg_match('/^HTTP_/',$name)) {
-                // convert HTTP_HEADER_NAME to Header-Name
-                $name = strtr(substr($name,5),'_',' ');
-                $name = ucwords(strtolower($name));
-                $name = strtr($name,' ','-');
-
-                // add to list
-                $headerList[$name] = $value;
-            }
-        }
-
-        return $headerList;
-    }
-}
-
-
-(new DumpHTTPRequestToFile)->execute('./dumprequest-'.microtime(true).'.txt');
-
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
@@ -71,8 +27,6 @@ function logwrite($text) {
 
 $body = file_get_contents('php://input');
 $data = json_decode($body);
-
-file_put_contents("request-".microtime(true).".json",var_export($data,true));
 
 try {
     $ninja = new InvoiceNinja($_ENV['NINJA_TOKEN']);
@@ -86,6 +40,7 @@ $clients = $ninja->clients->all(["per_page"=>9999999]);
 logwrite("Got ".sizeof($clients["data"])." clients");
 
 $thisclient = $data->NotificationUrl->object->Payment->counterparty_alias->display_name;
+$thisclient = "NEP Mediasolutions BV";
 logwrite("Looking for client ".$thisclient);
 $client = findBestMatchIndex(array_map(function($elm) { return $elm["name"]; },$clients["data"]),$thisclient);
 logwrite("Best matching client has index ".$client);
