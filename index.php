2c00dbd875c9078a8ed044de015c371148588483
Bas Casander <bas@breenstorm.nl>
2022-11-05 09:57:17 +0100

    All prepared to add actual payment api-call

--- index.php
+++ index.php
@@ -46,7 +46,6 @@ if ($searchiban == $data->NotificationUrl->object->Payment->alias->iban) {
     logwrite("Looking for client ".$thisclient);
     $transactionclient_id = findBestMatchIndex($thisclient,array_map(function($elm) { return $elm["name"]; },$clients["data"]));
     $transactionclient = $clients["data"][$transactionclient_id];
-    logwrite(var_export($transactionclient,true));
     logwrite("Best matching client has index ".$transactionclient_id." and name ". $transactionclient["name"]);
     $transactionamount = floatval($data->NotificationUrl->object->Payment->amount->value);
     $transactiondesc = $data->NotificationUrl->object->Payment->description;
@@ -97,8 +96,13 @@ if ($searchiban == $data->NotificationUrl->object->Payment->alias->iban) {
         ];
         logwrite(var_export($paymentparams,true));
         //TODO: Create and apply payment
-        $res = $ninja->payments->create($paymentparams);
-        logwrite(var_export($res,true));
+        try {
+            $res = $ninja->payments->create($paymentparams);
+            logwrite(var_export($res,true));
+        }
+        catch (Exception $e) {
+            logwrite($e->getMessage());
+        }
     } else {
         logwrite("No conclusive match found. Not applying payment.");
     }
